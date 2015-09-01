90756b69f02851727e826f3eaae840bc6333b099
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-01 09:45:00 +0000

    Created tag 1.2.9.
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-email-inquiry-cart-options/tags/1.2.9@1235269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin-ui.php
+++ admin/admin-ui.php
@@ -1,5 +1,5 @@
 <?php
-/* "Copyright 2012 A3 Revolution Web Design" This software is distributed under the terms of GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 */
+/* "Copyright 2012 a3 Revolution Web Design" This software is distributed under the terms of GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 */
 // File Security Check
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
@@ -31,35 +31,35 @@ class WC_Email_Inquiry_Admin_UI
 	 * You must change to correct plugin name that you are working
 	 */
 	public $plugin_name = 'wc_orders_quotes';
-	
+
 	public $is_free_plugin = true;
-	
+
 	/**
 	 * @var string
 	 * You must change to correct class name that you are working
 	 */
 	public $class_name = 'WC_Email_Inquiry';
-	
+
 	/**
 	 * @var string
 	 * You must change to correct pro plugin page url on a3rev site
 	 */
 	public $pro_plugin_page_url = 'http://a3rev.com/shop/woocommerce-quotes-and-orders/';
-	
+
 	public $ultimate_plugin_page_url = 'http://a3rev.com/shop/woocommerce-email-inquiry-ultimate/';
-	
+
 	public $profirst_plugin_page_url = 'http://a3rev.com/shop/woocommerce-email-inquiry-and-cart-options/';
-	
+
 	/**
 	 * @var string
 	 */
 	public $admin_plugin_url;
-	
+
 	/**
 	 * @var string
 	 */
 	public $admin_plugin_dir;
-	
+
 	/**
 	 * @var array
 	 * You must change to correct page you want to include scripts & styles, if you have many pages then use array() : array( 'quotes-orders-mode', 'quotes-orders-rule' )
@@ -74,7 +74,7 @@ class WC_Email_Inquiry_Admin_UI
 		if ( $this->admin_plugin_url ) return $this->admin_plugin_url;
 		return $this->admin_plugin_url = untrailingslashit( plugins_url( '/', __FILE__ ) );
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* admin_plugin_dir() */
 	/*-----------------------------------------------------------------------------------*/
@@ -82,16 +82,16 @@ class WC_Email_Inquiry_Admin_UI
 		if ( $this->admin_plugin_dir ) return $this->admin_plugin_dir;
 		return $this->admin_plugin_dir = untrailingslashit( plugin_dir_path( __FILE__ ) );
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* admin_pages() */
 	/*-----------------------------------------------------------------------------------*/
 	public function admin_pages() {
 		$admin_pages = apply_filters( $this->plugin_name . '_admin_pages', $this->admin_pages );
-		
+
 		return (array)$admin_pages;
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* plugin_extension_start() */
 	/* Start of yellow box on right for pro fields
@@ -99,15 +99,15 @@ class WC_Email_Inquiry_Admin_UI
 	public function plugin_extension_start( $echo = true ) {
 		$output = '<div id="a3_plugin_panel_container">';
 		$output .= '<div id="a3_plugin_panel_fields">';
-		
+
 		$output = apply_filters( $this->plugin_name . '_plugin_extension_start', $output );
-		
+
 		if ( $echo )
 			echo $output;
 		else
 			return $output;
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* plugin_extension_start() */
 	/* End of yellow box on right for pro fields
@@ -120,38 +120,38 @@ class WC_Email_Inquiry_Admin_UI
 		$output .= '</div>';
 		$output .= '</div>';
 		$output .= '</div>';
-		
+
 		$output = apply_filters( $this->plugin_name . '_plugin_extension_end', $output );
-		
+
 		if ( $echo )
 			echo $output;
 		else
 			return $output;
-		
+
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* upgrade_top_message() */
 	/* Show upgrade top message for pro fields
 	/*-----------------------------------------------------------------------------------*/
 	public function upgrade_top_message( $echo = false, $setting_id = '' ) {
-		$upgrade_top_message = sprintf( '<div class="pro_feature_top_message">' 
-			. __( 'Settings inside this yellow border are %s Features.', 'wc_email_inquiry' ) 
-			. '<br />' 
-			. __( 'Upgrade to the <a href="%s" target="_blank">%s</a> to activate these settings.', 'wc_email_inquiry' ) 
+		$upgrade_top_message = sprintf( '<div class="pro_feature_top_message">'
+			. __( 'Settings inside this yellow border are %s Features.', 'wc_email_inquiry' )
+			. '<br />'
+			. __( 'Upgrade to the <a href="%s" target="_blank">%s</a> to activate these settings.', 'wc_email_inquiry' )
 			. '</div>'
 			, apply_filters( $this->plugin_name . '_' . $setting_id . '_pro_version_name', apply_filters( $this->plugin_name . '_pro_version_name', __( 'Quotes & Orders Version', 'wc_email_inquiry' ) ) )
 			, apply_filters( $this->plugin_name . '_' . $setting_id . '_pro_plugin_page_url', apply_filters( $this->plugin_name . '_pro_plugin_page_url', $this->pro_plugin_page_url ) )
 			, apply_filters( $this->plugin_name . '_' . $setting_id . '_pro_version_name', apply_filters( $this->plugin_name . '_pro_version_name', __( 'Quotes & Orders Version', 'wc_email_inquiry' ) ) )
 		);
-		
+
 		$upgrade_top_message = apply_filters( $this->plugin_name . '_upgrade_top_message', $upgrade_top_message );
-		
+
 		if ( $echo ) echo $upgrade_top_message;
 		else return $upgrade_top_message;
-		
+
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* pro_fields_before() */
 	/* Start of yellow box on right for pro fields
@@ -159,7 +159,7 @@ class WC_Email_Inquiry_Admin_UI
 	public function pro_fields_before( $echo = true ) {
 		echo apply_filters( $this->plugin_name . '_pro_fields_before', '<div class="pro_feature_fields">'. $this->upgrade_top_message() );
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* pro_fields_after() */
 	/* End of yellow border for pro fields
@@ -167,7 +167,7 @@ class WC_Email_Inquiry_Admin_UI
 	public function pro_fields_after( $echo = true ) {
 		echo apply_filters( $this->plugin_name . '_pro_fields_after', '</div>' );
 	}
-	
+
 	/*-----------------------------------------------------------------------------------*/
 	/* blue_message_box() */
 	/* Blue Message Box
@@ -175,7 +175,7 @@ class WC_Email_Inquiry_Admin_UI
 	public function blue_message_box( $message = '', $width = '600px' ) {
 		$message = '<div class="a3rev_blue_message_box_container" style="width:'.$width.'"><div class="a3rev_blue_message_box">' . $message . '</div></div>';
 		$message = apply_filters( $this->plugin_name . '_blue_message_box', $message );
-		
+
 		return $message;
 	}
 
--- classes/class-wc-email-inquiry-hook.php
+++ classes/class-wc-email-inquiry-hook.php
@@ -16,7 +16,6 @@
  * wc_email_inquiry_popup()
  * wc_email_inquiry_action()
  * add_style_header()
- * include_customized_style()
  * footer_print_scripts()
  * script_contact_popup()
  * a3_wp_admin()
@@ -144,7 +143,6 @@ class WC_Email_Inquiry_Hook_Filter
 		$wc_email_inquiry_button_position = $wc_email_inquiry_customize_email_button['inquiry_button_position'];
 		
 		$wc_email_inquiry_button_class = '';
-		if ( trim( $wc_email_inquiry_customize_email_button['inquiry_button_class'] ) != '') $wc_email_inquiry_button_class = $wc_email_inquiry_customize_email_button['inquiry_button_class'];
 		
 		$button_link = '';
 		if (trim($wc_email_inquiry_text_before) != '') $button_link .= '<span class="wc_email_inquiry_text_before wc_email_inquiry_text_before_'.$product_id.'">'.trim($wc_email_inquiry_text_before).'</span> ';
@@ -213,7 +211,6 @@ class WC_Email_Inquiry_Hook_Filter
 	}
 	
 	public static function wc_email_inquiry_popup() {
-		check_ajax_referer( 'wc_email_inquiry_popup', 'security' );
 
 		global $wc_email_inquiry_contact_form_settings;
 		global $wc_email_inquiry_customize_email_button;
@@ -286,7 +283,6 @@ class WC_Email_Inquiry_Hook_Filter
 	}
 	
 	public static function wc_email_inquiry_action() {
-		check_ajax_referer( 'wc_email_inquiry_action', 'security' );
 		$product_id 	= esc_attr( stripslashes( $_REQUEST['product_id'] ) );
 		$your_name 		= esc_attr( stripslashes( $_REQUEST['your_name'] ) );
 		$your_email 	= esc_attr( stripslashes( $_REQUEST['your_email'] ) );
@@ -301,10 +297,6 @@ class WC_Email_Inquiry_Hook_Filter
 		
 	public static function add_style_header() {
 		wp_enqueue_style('a3_wc_email_inquiry_style', WC_EMAIL_INQUIRY_CSS_URL . '/wc_email_inquiry_style.css');
-	}
-	
-	public static function include_customized_style() {
-		include( WC_EMAIL_INQUIRY_DIR. '/templates/customized_style.php' );
 	}
 	
 	public static function footer_print_scripts() {
@@ -498,8 +490,9 @@ function ei_getWidth() {
 		$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
 		
 		wp_enqueue_script('jquery');
-		wp_enqueue_style( 'a3rev-chosen-style', $wc_ei_admin_interface->admin_plugin_url() . '/assets/js/chosen/chosen.css' );
-		wp_enqueue_script( 'chosen', $wc_ei_admin_interface->admin_plugin_url() . '/assets/js/chosen/chosen.jquery' . $suffix . '.js', array( 'jquery' ), true, false );
+		wp_enqueue_style( 'a3rev-chosen-new-style', $wc_ei_admin_interface->admin_plugin_url() . '/assets/js/chosen/chosen' . $suffix . '.css' );
+		wp_enqueue_script( 'a3rev-chosen-new', $wc_ei_admin_interface->admin_plugin_url() . '/assets/js/chosen/chosen.jquery' . $suffix . '.js', array( 'jquery' ), true, false );
+		wp_enqueue_style( 'a3rev-admin-flat-ui-style', $wc_ei_admin_interface->admin_plugin_url() . '/assets/css/admin-flat-ui-style' . $suffix . '.css' );
 	?>
 <script type="text/javascript">
 jQuery(document).ready(function() {
@@ -534,6 +527,12 @@ jQuery(document).ready(function() {
 		$links[] = '<a href="http://docs.a3rev.com/user-guides/woocommerce/woo-email-inquiry-cart-options/" target="_blank">'.__('Documentation', 'wc_email_inquiry').'</a>';
 		$links[] = '<a href="http://wordpress.org/support/plugin/woocommerce-email-inquiry-cart-options/" target="_blank">'.__('Support', 'wc_email_inquiry').'</a>';
 		return $links;
+	}
+
+	public static function settings_plugin_links($actions) {
+		$actions = array_merge( array( 'settings' => '<a href="admin.php?page=email-cart-options">' . __( 'Settings', 'wc_email_inquiry' ) . '</a>' ), $actions );
+
+		return $actions;
 	}
 }
 ?>
